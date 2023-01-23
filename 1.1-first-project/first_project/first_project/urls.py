4e328a30175d4a4d4c6b7667f43b7e2b6292b188
carchacq <n.karshak@gmail.com>
2023-01-23 12:26:37 +0300

    'final'

--- 1.1-first-project/first_project/first_project/urls.py
+++ 1.1-first-project/first_project/first_project/urls.py
@@ -17,14 +17,14 @@ from django.contrib import admin
 from django.urls import path, include
 
 from app.views import home_view
-from app.views import time
+from app.views import time_view
+from app.views import workdir_view
+
 
 
 urlpatterns = [
     path('', home_view, name='home'),
-    # Раскомментируйте код, чтобы данные урлы 
-    # обрабатывались Django
     path('current_time/', time_view, name='time'),
-    # path('workdir/', workdir_view, name='workdir'),
+    path('workdir/', workdir_view, name='workdir'),
     path('admin/', admin.site.urls),
 ]
