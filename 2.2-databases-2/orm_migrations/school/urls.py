87e080a03db8373dca61b1fc3dfff958bfca5428
carchacq <n.karshak@gmail.com>
2023-02-09 16:49:33 +0300

    'final'

--- 2.2-databases-2/orm_migrations/school/urls.py
+++ 2.2-databases-2/orm_migrations/school/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from school.views import students_list
+from school.views import students_list, add_teacher
 
 urlpatterns = [
     path('', students_list, name='students'),
+    path('add', add_teacher)
 ]
--- 2.2-databases-2/orm_migrations/school/views.py
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -1,15 +1,21 @@
 from django.views.generic import ListView
+from django.http import HttpResponse
 from django.shortcuts import render
-
-from .models import Student
+import random
+from .models import Student, Teacher
 
 
 def students_list(request):
     template = 'school/students_list.html'
-    context = {}
-
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = 'group'
-
+    students = Student.objects.all().prefetch_related('teachers').order_by('group')
+    context = {'object_list': students}
     return render(request, template, context)
+
+def add_teacher(request):
+    students = Student.objects.all()
+    teachers = Teacher.objects.all()
+    for student in students:
+        random = teachers.order_by('?').first()
+        student.teachers.add(random)
+    return HttpResponse('Учителя назначены')
+
